34260ef6f48dd970f5257821210313dd394a7fcd
Mark Williams <williamsmp25@gmail.com>
2017-09-27 14:56:43 +1300

    Testing api calls, troubleshooting database (ldf file missing)

--- refactor-me/Models/Products.cs
+++ refactor-me/Models/Products.cs
@@ -37,14 +37,11 @@ namespace refactor_me.Models
 
     public class Product
     {
+        // product fields
         public Guid Id { get; set; }
-
         public string Name { get; set; }
-
         public string Description { get; set; }
-
         public decimal Price { get; set; }
-
         public decimal DeliveryPrice { get; set; }
         
         [JsonIgnore]
